4d1557e33930ac7cff65e50dedb1a0462cecacfc
Kevan Hollbach <khollbach@gmail.com>
2023-10-12 19:32:53 -0400

    PPM hello world

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 fn main() {
-    print_circle(2.5);
+    print_red_circle_ppm(2.5);
 }
 
-fn print_circle(r: f64) {
+// TODO: gradient ?
+
+fn print_red_circle_ppm(r: f64) {
     const X: i32 = 64;
     const Y: i32 = 48;
+
+    println!("P3");
+    println!("{X} {Y} 255");
+    println!();
+
     for y in 0..Y {
         for x in 0..X {
             let dx = x - X / 2;
             let dy = y - Y / 2;
             let hit = ((dx.pow(2) + dy.pow(2)) as f64) < r.powf(2.);
-            let symbol = if hit { 'x' } else { '.' };
-            print!("{symbol}");
+            let color = if hit { "255 0 0" } else { "0 0 0" };
+            println!("{color}");
         }
         println!();
     }

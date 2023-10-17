835e8a35fda30cd23669323cbd52c431ed296c3c
Kevan Hollbach <khollbach@gmail.com>
2023-10-17 10:33:11 -0400

    Add a Cornell Box scene (no objects in the box)

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ mod scene;
 mod sdl;
 mod vec3;
 
-use std::fs;
+use std::{fs, env};
 
 use error::Result;
 use scene::Scene;
 
 fn main() -> Result<()> {
-    let text = fs::read_to_string("scene.sdl")?;
+    let mut args = env::args().skip(1);
+    if args.len() != 1 {
+        Err("expected 1 argument: <scene-file.sdl>")?
+    }
+    let filename = args.next().unwrap();
+    let text = fs::read_to_string(filename)?;
     Scene::from_sdl(&text)?.render();
     Ok(())
 }
